5ad07a266fb643303aa91b5dfe89b93cdae46e53
Pierre CLEMENT <pierrecle@gmail.com>
2015-04-14 01:51:01 +0200

    +confs

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        typescript: {
+            base: {
+                src: ['src/**/*.ts'],
+                options: {
+                    target: 'es5'
+                }
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        },
+        watch: {
+            // Call globhtml when change occurs
+            typescript: {
+                files: ['**/*.ts'],
+                tasks: ['typescript']
+            },
+            karma: {
+                files: ['src/**/*.js', 'tests/**/*.js'],
+                tasks: ['karma']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-karma');
+};
